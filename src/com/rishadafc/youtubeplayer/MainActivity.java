332760ded037e9494fca839811ef77fe12805508
Rishad Bharucha <rb2102@gmail.com>
2015-01-02 13:01:26 +0530

    Add Play Video button

--- src/com/rishadafc/youtubeplayer/MainActivity.java
+++ src/com/rishadafc/youtubeplayer/MainActivity.java
@@ -1,17 +1,30 @@
 package com.rishadafc.youtubeplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+	
+	Button btnPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, YouTubeActivity.class);
+				startActivity(intent);
+			}
+		});
     }
 
 
